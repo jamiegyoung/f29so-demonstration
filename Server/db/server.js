b179c7841e5d7a2ce40b4442debcbeccc4a7aad6
ralf <rstewart542@gmail.com>
2022-01-20 23:31:09 +0000

    Changes to files

--- Server/db/server.js
+++ Server/db/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const db = require('./app/Backend/database')
+const db = require('./database')
 const port = 3000
 const passport = require('passport')
 
