f424c943379e28546fe960d9a5e4e5d6cf0b5ed0
jamiegyoung <jamiegyoung22@gmail.com>
2022-03-30 03:24:05 +0100

    Add feed information

--- app/src/features/feed/Feed.tsx
+++ app/src/features/feed/Feed.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { clearFeed, fetchUserFeed } from './feedSlice';
 import { FeedPost } from '../../types';
 import WallPost from '../../components/WallPost';
+import Spinner from '../../components/Spinner';
 
 function Feed() {
   const feedData = useAppSelector((state) => state.feed);
@@ -20,19 +21,31 @@ function Feed() {
 
   return (
     <div className={styles.feed}>
-      {feedData?.posts.map((post: FeedPost) => (
-        <WallPost
-          key={post.wallID}
-          wallID={post.wallID}
-          ownerID={post.ownerID}
-          ownerUsername={post.ownerUsername}
-          edits={post.edits}
-          likes={post.likes}
-          lastEdit={post.lastEdit}
-          preview={post.preview}
-          liked={post.liked}
-        />
-      ))}
+      {feedData.status === 'success' && feedData.posts.length > 0
+        ? feedData?.posts.map((post: FeedPost) => (
+            <WallPost
+              key={post.wallID}
+              wallID={post.wallID}
+              ownerID={post.ownerID}
+              ownerUsername={post.ownerUsername}
+              edits={post.edits}
+              likes={post.likes}
+              lastEdit={post.lastEdit}
+              preview={post.preview}
+              liked={post.liked}
+            />
+          ))
+        : null}
+      {feedData.status === 'success' && feedData.posts.length === 0 ? (
+        <p>
+          Looks like there&apos;s nothing here! You can start by creating a new
+          wall or by following someone.
+        </p>
+      ) : null}
+      {feedData.status === 'error' ? (
+        <p>There was an error fetching these WALLS. Please try again later.</p>
+      ) : null}
+      {feedData.status === 'loading' ? <Spinner /> : null}
     </div>
   );
 }
